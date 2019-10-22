8ce9036e1d3c73c3345fc91badc8d4b1de273be8
cycjimmy <cycjimmy@gmail.com>
2019-10-22 14:52:30 +0800

    fix(release): modify release config

--- release.config.js
+++ release.config.js
@@ -1,15 +1,15 @@
 const makeConfig = require('@cycjimmy/config-lib/semanticRelease/15.x/makeConfig');
 
 module.exports = makeConfig({
-  exec : true,
-  execOptions:{
+  exec: true,
+  execOptions: {
     prepareCmd: 'npm run package'
   },
   npmOptions: {
     pkgRoot: 'dist'
   },
   git: true,
-  gitAssets: gitAssets = [
+  gitAssets: [
     "package.json",
     "package-lock.json"
   ]
