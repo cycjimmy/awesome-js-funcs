4b3a515c990b18ed0bbc6106bc0e2e2549b3857e
cycjimmy <cycjimmy@gmail.com>
2022-03-25 21:18:42 +0800

    fix: fix root version after releasing

--- release.config.cjs
+++ release.config.cjs
@@ -1,12 +1,4 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-const makeCommonConfig = require('@cycjimmy/config-lib/cjs/semanticRelease/15.x/makeCommonConfig.cjs').default;
+const makeConfig = require('@cycjimmy/config-lib/cjs/semanticRelease/19.x/makeConfigWithPgkRootForLibrary.cjs').default;
 
-module.exports = makeCommonConfig({
-  exec: true,
-  execOptions: {
-    prepareCmd: 'npm run package',
-  },
-  npmOptions: {
-    pkgRoot: '.release',
-  },
-});
+module.exports = makeConfig();
